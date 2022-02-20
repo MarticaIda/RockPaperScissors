ec6365f73c26347f63dc150e089e0ddccc52f614
MarticaIda <94822301+MarticaIda@users.noreply.github.com>
2022-02-20 16:31:52 -0600

    created score
    need to work on strict user input

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let playerScore = 0
 let computerScore = 0
 
+
 function playRound() {
     const computerSelection = computerPlay()
-    const playerSelection = prompt("Make your selection").toLowerCase()
 
+    const playerSelection = prompt("Make your selection").toLowerCase()
 
     function computerPlay() {
         let hand = ["rock", "paper", "scissors"]
@@ -12,21 +13,27 @@ function playRound() {
         return hand[random]
     }
     if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
         return ("You win! Rock beats scissors")
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++
         return ("You lose! Rock beats scissors")
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++
         return ("You lose! Scissors beat paper")
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
         return ("You win! Scissors beats paper")
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++
         return ("You lose! Paper beats rock")
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
         return ("You win! Paper beats rock")
     }
     else {
@@ -37,8 +44,16 @@ function playRound() {
 function game() {
     console.log("game starts")
     for (let i = 1; i <= 5; i++) {
-        console.log('i: ' + i.toString())
         console.log(playRound())
+    }
+    if (playerScore > computerScore) {
+        console.log("You won!")
+    }
+    else if (playerScore===computerScore){
+        console.log("It's a draw!")
+    }
+    else {
+        console.log("You lost!")
     }
 }
 
