e598619ff3a9876831e5c36c488791afb11681b3
Maria Martinkous <marticaida@gmail.com>
2022-06-25 15:52:06 -0500

    changed playerSelections

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 let playerScore = 0;
 let computerScore = 0;
-//let playerPrompt;
-//let validChoices = [];
-const buttonRock = document.getElementById('buttonRock').addEventListener('click', playRound);
-const buttonPaper = document.getElementById('buttonPaper').addEventListener('click', playRound);
-const buttonScissors = document.getElementById('buttonScissors').addEventListener('click', playRound);
+const buttonRock = document.getElementById('buttonRock').addEventListener('click', playRoundRock);
+const buttonPaper = document.getElementById('buttonPaper').addEventListener('click', playRoundPaper);
+const buttonScissors = document.getElementById('buttonScissors').addEventListener('click', playRoundScissors);
 
-function playRound() {
+function playRoundRock() {
   const computerSelection = computerPlay();
   const playerSelectionRock = 'rock';
-  const playerSelectionPaper = 'paper';
-  const playerSelectionScissors = 'scissors';
-
 
   function computerPlay() {
     const hand = ['rock', 'paper', 'scissors'];
@@ -19,42 +14,55 @@ function playRound() {
     return hand[random];
   };
 
-  // function promptForSelection() {
-  //   validChoices = ['rock', 'paper', 'scissors'];
-  //   playerPrompt = prompt('Make your selection');
-  //   if (playerPrompt != null) {
-  //     playerPrompt = playerPrompt.toLowerCase();
-  //   } else {
-  //     alert('Game aborted');
-  //     throw new Error('Game aborted');
-  //   };
-
-  //   while (validChoices.indexOf(playerPrompt) === -1) {
-  //     alert('Not a valid choice. Pick again');
-  //     playerPrompt = promptForSelection();
-  //   }
-  //   return playerPrompt;
-  // };
-
   if (playerSelectionRock && computerSelection === 'scissors') {
     playerScore++
     return ('You win! Rock beats scissors');
-  } else if (playerSelectionScissors && computerSelection === 'rock') {
-    computerScore++
-    return ('You lose! Rock beats scissors');
-  } else if (playerSelectionPaper && computerSelection === 'scissors') {
-    computerScore++
-    return ('You lose! Scissors beat paper');
-  } else if (playerSelectionScissors && computerSelection === 'paper') {
-    playerScore++
-    return ('You win! Scissors beats paper');
   } else if (playerSelectionRock && computerSelection === 'paper') {
     computerScore++
     return ('You lose! Paper beats rock');
+  } else if (playerSelectionRock === computerSelection) {
+    return ('Draw!');
+  };
+};
+
+function playRoundPaper() {
+  const computerSelection = computerPlay();
+  const playerSelectionPaper = 'paper';
+
+  function computerPlay() {
+    const hand = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * hand.length);
+    return hand[random];
+  };
+
+  if (playerSelectionPaper && computerSelection === 'scissors') {
+    computerScore++
+    return ('You lose! Scissors beat paper');
   } else if (playerSelectionPaper && computerSelection === 'rock') {
     playerScore++
     return ('You win! Paper beats rock');
-  } else if (playerSelection === computerSelection) {//need to introduce another playerSelection-computerSelection comparison
+  } else if (playerSelectionPaper === computerSelection) {
+    return ('Draw!');
+  };
+};
+
+function playRoundScissors() {
+  const computerSelection = computerPlay();
+  const playerSelectionScissors = 'scissors';
+
+  function computerPlay() {
+    const hand = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * hand.length);
+    return hand[random];
+  };
+
+  if (playerSelectionScissors && computerSelection === 'rock') {
+    computerScore++
+    return ('You lose! Rock beats scissors');
+  } else if (playerSelectionScissors && computerSelection === 'paper') {
+    playerScore++
+    return ('You win! Scissors beats paper');
+  } else if (playerSelectionScissors === computerSelection) {
     return ('Draw!');
   };
 };
