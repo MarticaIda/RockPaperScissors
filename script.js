aafa4c83a8f33c2480e957f461a446eb64c350b2
Maria Martinkous <marticaida@gmail.com>
2022-06-27 17:49:02 -0500

    fixed formatting

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
-const playerScore = 0;
+const userScore = 0;
 const computerScore = 0;
 let result = document.getElementById('result');
 
-function playRound() {
+function playGame() {
   do {
-    function computerPlay() {
+    function playRound() {
+      const userSelection = userPlay();
+      function userPlay() {
+        const buttonRock = document.getElementById('button-rock').addEventListener('click', function chooseRock() {
+          return userSelection = 'rock';
+          });
+        const buttonPaper = document.getElementById('button-paper').addEventListener('click', function choosePaper() {
+          return userSelection = 'paper';
+          });
+        const buttonScissors = document.getElementById('button-scissors').addEventListener('click', function chooseScissors() {
+          return userSelection = 'scissors';
+          });
+      }
+      const computerSelection = computerPlay();
+      function computerPlay() {
       const hand = ['rock', 'paper', 'scissors'];
       const random = Math.floor(Math.random() * hand.length);
-      return hand[random];
+      return hand[random]
     }
-    while (i<=playerScore || i<=computerScore);
-  }
-}
-function playGame() {
-  // keep track of score until it reaches 5
-  // for every round invite player to click on choice button
-  // player clicks on the choice button and computer makes a choice
-  // if statement declares who wins round
-  // score is incremented and displayed
-  // when one score reaches 5 game stops
-  // result is displayed
-  return (result = `Player Score: ${playerScore} Computer Score: ${computerScore}`)
-};
+    }
+    if (userSelection === 'rock' && computerSelection === 'scissors') {
+      userScore++;
+      return userScore;
+    } else if (userSelection === 'scissors' && computerSelection === 'rock') {
+      computerScore++;
+      return computerScore;
+    } else if (userSelection === 'paper' && computerSelection === 'scissors') {
+      computerScore++;
+      return computerScore;
+    } else if (userSelection === 'scissors' && computerSelection === 'paper') {
+      userScore++;
+      return userScore;
+    } else if (userSelection === 'rock' && computerSelection === 'paper') {
+      computerScore++;
+      return computerScore;
+    } else if (userSelection === 'paper' && computerSelection === 'rock') {
+      userScore++;
+      return userScore;
+    } else if (userSelection === computerSelection) {
+      return ('Draw!');
+    }
+  while (i <= userScore || i <= computerScore)
 
+// keep track of score until it reaches 5 DONE
+// for every round invite user to click on choice button
+// user clicks on the choice button and computer makes a choice DONE
+// if statement declares who wins round DONE
+// score is incremented and displayed
+// when one score reaches 5 game stops
+// result is displayed
+  return (result = `user Score: ${userScore} Computer Score: ${computerScore}`);
+  };
+};
 playGame();
